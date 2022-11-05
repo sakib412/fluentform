044e6eee6bb44268cea094b3c42168b5529b67bf
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2022-11-05 20:35:12 +0600

    populate text max length option

--- app/Services/Migrator/Classes/WpFormsMigrator.php
+++ app/Services/Migrator/Classes/WpFormsMigrator.php
@@ -287,6 +287,15 @@ class WpFormsMigrator extends BaseMigrator
         ];
         
         switch ($type) {
+            case 'input_text':
+                $max_length = ArrayHelper::get($field, 'limit_count', '');
+                if ($max_length && $mode = ArrayHelper::get($field, 'limit_mode')) {
+                    if ("words" == $mode && is_string($max_length)) {
+                        $max_length = (int)$max_length * 6; // average 6 characters is a word
+                    }
+                    $args['maxlength'] = $max_length;
+                }
+                break;
             case 'phone':
                 $args['valid_phone_number'] = "1";
                 break;
