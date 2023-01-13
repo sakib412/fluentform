9ca21499500ab1a1a1d724d0f95a39c3f8d8e241
Lukman Nakib <48018765+nkb-bd@users.noreply.github.com>
2023-01-13 14:25:39 +0600

    Added Email Input and Message in Save Resume Button

--- resources/assets/public/form-save-progress.js
+++ resources/assets/public/form-save-progress.js
@@ -20,13 +20,14 @@ import formSlider from "./Pro/slider";
             $theForm.on('ff_to_prev_page', function (e, currentStep) {
                 activeStep = currentStep;
             });
-
         }
-
 
         $(formSelector).on('click', '.ff-btn-save-progress', function (e) {
             e.preventDefault();
-            var $inputs = $theForm.find(':input').filter(function (i, el) {
+            let $saveBttn = $(this);
+            $saveBttn.addClass('ff-working');
+
+            const $inputs = $theForm.find(':input').filter(function (i, el) {
                 return !$(el).closest('.has-conditions').hasClass('ff_excluded');
             });
 
@@ -70,6 +71,7 @@ import formSlider from "./Pro/slider";
                 hash: hash,
                 active_step: activeStep
             };
+
             jQuery.post(fluentFormVars.ajaxUrl, formData).then(data => {
                 if (data) {
                     hash = data.data.hash;
@@ -79,16 +81,111 @@ import formSlider from "./Pro/slider";
                         return;
                     }
 
-                    let label = '<div class="ff-el-input--label"><label>Copy Link</label></div>';
-                    let inputDiv = `<div class="ff-el-input--content"><input readonly value="${data.data.saved_url}" class="ff-el-form-control" ></div>`;
-                    let inputGroup = $('<div/>', {class: 'ff-el-group ff-saved-state-link', html: label + inputDiv});
+
+                    const saveProgressMessage = formData.form_id + '_save_progress_msg';
+                    const successMsgSelector = '#' + saveProgressMessage;
+                    if ($(successMsgSelector).length) {
+                        $(successMsgSelector).slideUp('fast');
+                    }
+                    $('<div/>', {
+                        'id': saveProgressMessage,
+                        'class': 'ff-message-success ff-el-group'
+                    })
+                        .html(data.data.message)
+                        .insertBefore($saveBttn.closest('.ff-el-group'));
+
+
+                    //Show Link in Input
+                    let inputDiv = `<div class="ff-el-input--content"><div class="ff_input-group"><input readonly value="${ data.data.saved_url }" class="ff-el-form-control" >
+                    <div class="ff_input-group-append">
+                    <button class="ff-btn ff-btn-md ff_btn_style ff_btn_copy_link ff_input-group-text">
+                        ${window.form_state_save_vars.copy_button}
+                    </button></div></div></div>`;
+                    let inputGroup = $('<div/>', { class: 'ff-el-group ff-saved-state-input ff-saved-state-link ff-hide-group', html: inputDiv });
 
                     $(this).closest('.ff-el-group').after(
                         inputGroup
                     )
+                    inputGroup.fadeIn();
+
+                    //Show Email Input
+                    if ($(this).hasClass('ff_resume_email_enabled')) {
+                        let emailDiv = `<div class="ff-el-input--content"><div class="ff_input-group"><input type="email" class="ff-el-form-control" placeholder="Your Email Here" class="ff-el-form-control">
+                        <div class="ff_input-group-append">
+                        <button class="ff-btn ff-btn-md ff_btn_style ff_btn_is_email ff_input-group-text">
+                             ${window.form_state_save_vars.email_button}
+                        </button></div></div></div>`;
+                        let emailGroup = $('<div/>', { class: 'ff-el-group ff-saved-state-input  ff-email-address ff-hide-group', html: emailDiv });
+
+                        $(inputGroup).after(
+                            emailGroup
+                        )
+                        emailGroup.fadeIn();
+
+
+                    }
+                }
+            }).always(function () {
+                $saveBttn.parent().hide();
+            });
+        });
+
+        $(formSelector).on('click', '.ff_btn_copy_link', function (e) {
+            e.preventDefault();
+            let copiedText = $(this).closest('.ff-el-input--content').find('.ff-el-form-control').val();
+            navigator.clipboard.writeText(copiedText);
+            $(this).html(`${window.form_state_save_vars.copy_success_button}`);
+        });
+
+        $(formSelector).on('click', '.ff_btn_is_email', function (e) {
+            e.preventDefault();
+            const emailBtn = $(this).closest('.ff-el-group');
+            const to_email = $(this).closest('.ff-email-address').find('input').val();
+            $('.ff-email-address').find('input').val('');
+            const link = $('.ff-saved-state-link').find('input').val();
+            const formData = {
+                source_url: window.form_state_save_vars.source_url,
+                action: 'fluentform_email_progress_link',
+                form_id: $theForm.data('form_id'),
+                to_email: to_email,
+                link: link,
+                hash: hash
+            };
+            const emailResponse = formData.form_id + '_save_progress_email_response';
+            const responseMessageSelector = '#' + emailResponse;
+
+            jQuery.post(fluentFormVars.ajaxUrl, formData).then(data => {
+
+                if (data) {
+                    emailBtn.removeClass('ff-el-is-error')
+
+                    if ($(responseMessageSelector).length) {
+                        $(responseMessageSelector).slideUp('fast');
+                    }
+                    $('<div/>', {
+                        'id': emailResponse,
+                        'class': 'ff-message-success ff-el-group'
+                    })
+                        .html(data.data.response)
+                        .insertAfter(emailBtn);
+
+                }
+            }).fail(error => {
+                if (error) {
+
+                    emailBtn.addClass('ff-el-is-error')
+
+                    if ($(responseMessageSelector).length) {
+                        $(responseMessageSelector).slideUp('fast');
+                    }
+                    $('<div/>', {
+                        'id': emailResponse,
+                        'class': 'ff-message-success ff-el-group text-danger'
+                    })
+                        .html(error.responseJSON.data.Error)
+                        .insertAfter(emailBtn);
                 }
             });
-
         });
 
         //load data
@@ -99,7 +196,7 @@ import formSlider from "./Pro/slider";
         if (!hashKey) {
             return;
         }
-        $theForm.append(`<input type="hidden" value="${hashKey}" class="__fluent_state_hash" name="__fluent_state_hash"/>`)
+        $theForm.append(`<input type="hidden" value="${ hashKey }" class="__fluent_state_hash" name="__fluent_state_hash"/>`)
 
         jQuery.getJSON(fluentFormVars.ajaxUrl, {
             form_id: $theForm.data('form_id'),
