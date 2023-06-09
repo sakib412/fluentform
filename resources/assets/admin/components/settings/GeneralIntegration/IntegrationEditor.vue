70a06b4b00a929120b655230565a8a1e311d124f
Dhrupo Nil <dhrupo@gmail.com>
2023-06-09 15:23:08 +0600

    Airtable Improvement

--- resources/assets/admin/components/settings/GeneralIntegration/IntegrationEditor.vue
+++ resources/assets/admin/components/settings/GeneralIntegration/IntegrationEditor.vue
@@ -231,7 +231,7 @@
                                 <template v-else-if="field.component == 'chained-ajax-fields'">
                                     <template v-for="(optionValue, optionKey) in field.options_labels">
                                         <el-select
-                                                class="w-100"
+                                                class="w-100 mb-2"
                                                 v-loading="loading_list"
                                                 @change="chainedAjax(optionKey)"
                                                 v-model="settings.chained_config[optionKey]"
