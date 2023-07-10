3fdfbeeb39e04fbac7f3c6edc9839dd76c0b6f96
Lukman Nakib <lukman.nakib@gmail.com>
2023-07-10 11:48:34 +0600

    Moved analytics from controller

--- app/Modules/Component/Component.php
+++ app/Modules/Component/Component.php
@@ -712,7 +712,7 @@ class Component
             $feedText = apply_filters('fluentform/shortcode_feed_text', $feedText, $form);
             return $feedText;
         }
-
+        
         $formSettings = wpFluent()
             ->table('fluentform_form_meta')
             ->where('form_id', $form_id)
@@ -933,7 +933,7 @@ class Component
 
         if (!$this->app->applyFilters('fluentform/disabled_analytics', $disableAnalytics)) {
             if (!Acl::hasAnyFormPermission($form->id)) {
-                (new \FluentForm\App\Http\Controllers\AnalyticsController())->store($form->id);
+                (new \FluentForm\App\Services\Analytics\AnalyticsService())->store($form->id);
             }
         }
 
